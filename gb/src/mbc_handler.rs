8d728865162d1517fe81e995cea55e201534d047
Alon Cohen-Magen <alloncm@gmail.com>
2022-02-18 16:16:21 +0200

    Remove the need to have a .gb suffix for the rom

--- gb/src/mbc_handler.rs
+++ gb/src/mbc_handler.rs
@@ -3,14 +3,12 @@ use std::boxed::Box;
 use std::fs;
 use log::info;
 
-const PROGRAM_SUFFIX:&str = ".gb";
 pub const SAVE_SUFFIX:&str = ".sav";
 
 pub fn initialize_mbc(program_name:&String)->Box<dyn Mbc>{
 
-    let program_path = format!("{}{}",program_name,PROGRAM_SUFFIX);
-    let error_message = format!("No program found, notice that the file must have a `.gb` suffix - {}\n", program_name);
-    let program = fs::read(program_path).expect(error_message.as_str());
+    let error_message = format!("No program found - {}\n", program_name);
+    let program = fs::read(program_name).expect(error_message.as_str());
     let save_data = try_get_save_data(program_name);
     
     return lib_gb::machine::mbc_initializer::initialize_mbc(program, save_data);
